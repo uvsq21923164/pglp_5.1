b98f26e16667c049391ca4562612dc3eb7ba0e10
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-04-18 16:39:17 +0200

    creation l interface DAO

--- exo51/src/main/java/Uvsq21923164/exo51/DAO.java
+++ exo51/src/main/java/Uvsq21923164/exo51/DAO.java
@@ -3,11 +3,13 @@ package Uvsq21923164.exo51;
 public interface DAO<T> {
 		
 	/*
+	 * **
 	 * CRUD:
 	 *  - Create
 	 *  - Read
 	 *  - Update
 	 *  - Delete
+	 *  **
 	 */
 	T create(T obj);
 	T read(String id);
