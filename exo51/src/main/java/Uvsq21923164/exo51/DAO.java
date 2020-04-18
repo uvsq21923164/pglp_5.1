b80118c1a6df8c9eae10787997d46e35d680aa42
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-04-18 16:32:42 +0200

    ajoute la classe DAO

---
+++ exo51/src/main/java/Uvsq21923164/exo51/DAO.java
@@ -0,0 +1,17 @@
+package Uvsq21923164.exo51;
+
+public interface DAO<T> {
+		
+	/*
+	 * CRUD:
+	 *  - Create
+	 *  - Read
+	 *  - Update
+	 *  - Delete
+	 */
+	T create(T obj);
+	T read(String id);
+	T update(T obj);
+	void delete(T obj);
+
+}
