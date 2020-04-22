48daa968051c62294b01ad2fda52f599faa1b3ce
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-04-22 13:39:39 +0200

    correction

--- exo51/src/main/java/Uvsq21923164/exo51/DAO.java
+++ exo51/src/main/java/Uvsq21923164/exo51/DAO.java
@@ -2,15 +2,7 @@ package Uvsq21923164.exo51;
 
 public interface DAO<T> {
 		
-	/*
-	 * **
-	 * CRUD:
-	 *  - Create
-	 *  - Read
-	 *  - Update
-	 *  - Delete
-	 *  **
-	 */
+	
 	T create(T obj);
 	T read(String id);
 	T update(T obj);
